3fec225316c55260387a506b9f78ca58551f772f
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-03-25 23:01:05 +0100

    Dodanie testu klasy Money porownujacej wartosc z wartoscia oczekiwaną. Oczekiwane TRUE

--- src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -3,12 +3,16 @@ package pl.com.bottega.ecommerce.sharedkernel;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
 public class MoneyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_equals_expected_value_shouldBeTrue() {
+		Money money1 = new Money(10,"$");
+		Money expected_value = new Money(10,"$");
+		
+		assertThat(money1,is(equalTo(expected_value)));
 	}
 
 }
