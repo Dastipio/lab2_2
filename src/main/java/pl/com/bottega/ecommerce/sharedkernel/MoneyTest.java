71c767bb896c209443930ae203856e9e5c4bef01
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-03-25 23:19:25 +0100

    test - sprawdzenie metody less_than

--- src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -9,36 +9,44 @@ public class MoneyTest {
 
 	@Test
 	public void test_equals_expected_value_shouldBeTrue() {
-		Money money1 = new Money(10,"$");
-		Money expected_value = new Money(10,"$");
-		
-		assertThat(money1,is(equalTo(expected_value)));
+		Money money1 = new Money(10, "$");
+		Money expected_value = new Money(10, "$");
+
+		assertThat(money1, is(equalTo(expected_value)));
 	}
-	
+
 	@Test
 	public void test_add_10_and_10_shouldReturn20() {
 		Money money1 = new Money(10, "$");
 		Money money2 = new Money(10, "$");
 		Money expected = new Money(20, "$");
 
-		assertThat(money1.add(money2),is(equalTo(expected)));
+		assertThat(money1.add(money2), is(equalTo(expected)));
 	}
-	
-	
+
 	@Test
 	public void test_subtract_20_and_10_shouldReturn10() {
-		Money money1 = new Money(10,"$");
-		Money money2 = new Money(20,"$");
-		Money expected = new Money(10,"$");
-		
-		assertThat(money2.subtract(money1),is(equalTo(expected)));
+		Money money1 = new Money(10, "$");
+		Money money2 = new Money(20, "$");
+		Money expected = new Money(10, "$");
+
+		assertThat(money2.subtract(money1), is(equalTo(expected)));
 	}
 
 	@Test
 	public void test_multiply_10_and_10_ShouldReturn100() {
-		Money money1 = new Money(10,"PLN");
-		Money expected = new Money(100,"PLN");
-		
-		assertThat(money1.multiplyBy(10),is(equalTo(expected)));
+		Money money1 = new Money(10, "$");
+		Money expected = new Money(100, "$");
+
+		assertThat(money1.multiplyBy(10), is(equalTo(expected)));
 	}
+
+	@Test
+	public void test_lessThan_money1_to_money2ShouldReturnTrue() {
+		Money money1 = new Money(10, "$");
+		Money money2 = new Money(20, "$");
+
+		assertThat(money1.lessThan(money2), is(true));
+	}
+
 }
